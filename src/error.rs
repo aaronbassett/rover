add84e10040380cecea83e55ae7400d494655893
Aaron Bassett <aaronbassett@gmail.com>
2026-05-07 11:11:38 +0100

    feat(fetcher): SSRF Strict-level URL and address validation

--- src/error.rs
+++ src/error.rs
@@ -11,6 +11,9 @@ pub enum Error {
     #[error("config error: {0}")]
     Config(#[from] crate::config::ConfigError),
 
+    #[error("fetcher error: {0}")]
+    Fetcher(#[from] crate::fetcher::FetcherError),
+
     #[error("io error: {0}")]
     Io(#[from] std::io::Error),
 }
--- src/fetcher/mod.rs
+++ src/fetcher/mod.rs
@@ -1,1 +1,12 @@
 //! HTTP fetching, charset detection, SSRF enforcement.
+
+pub mod ssrf;
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum FetcherError {
+    #[error("ssrf violation: {0}")]
+    Ssrf(#[from] ssrf::SsrfError),
+    // More variants added in subsequent tasks.
+}
